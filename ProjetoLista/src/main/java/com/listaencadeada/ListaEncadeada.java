0e151cbf941d218304669d3cf493b637a574aeed
Angelo Carvalho <angelosuporteti@gmail.com>
2021-10-11 14:48:15 -0300

    Finalizando implementação da lista encadeada

--- ProjetoLista/src/main/java/com/listaencadeada/ListaEncadeada.java
+++ ProjetoLista/src/main/java/com/listaencadeada/ListaEncadeada.java
@@ -24,6 +24,35 @@ public class ListaEncadeada<T> {
         noAuxiliar.setProximoNo(novoNo);
     }
 
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+
+        validaIndice(index);
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+
+        for (int i = 0; i <= index; i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        return noRetorno;
+    }
+
+    public T remove(int index){
+        No<T> noPivor =  this.getNo(index);
+        if(index== 0){
+            referenciaEntrada = noPivor.getProximoNo();
+            return noPivor.getConteudo();
+        }
+        No<T> noAnterior = getNo(index -1);
+        noAnterior.setProximoNo(noPivor.getProximoNo());
+        return noPivor.getConteudo();
+    }
+
     public int size(){
         int tamanhoLista = 0;
         No<T> referenciaAux = referenciaEntrada;
@@ -44,7 +73,29 @@ public class ListaEncadeada<T> {
         return  tamanhoLista;
     }
 
+    private  void validaIndice(int index){
+        if(index >= size()){
+            int ultimoIndice = size( )-1;
+            throw new IndexOutOfBoundsException("Não existe conteúdo" + index + " desta lista. Esta lista vai até " + ultimoIndice + ".");
+        }
+
+    }
     public boolean isEmpty(){
         return referenciaEntrada == null? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+
+        for(int i = 0; i < this.size(); i++){
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        //ultimo nó
+        strRetorno += "null";
+        return strRetorno;
     }
 }
---
+++ ProjetoLista/src/main/java/com/listaencadeada/Main.java
@@ -0,0 +1,21 @@
+package com.listaencadeada;
+
+public class Main {
+    public static void main(String[] args){
+        ListaEncadeada<String> lista = new ListaEncadeada<>();
+
+        lista.add("primeiro");
+        lista.add("segundo");
+        lista.add("terceiro");
+        lista.add("quarto");
+        lista.add("quinto");
+
+        System.out.println(lista.get(0));
+        System.out.println(lista.get(1));
+        System.out.println(lista.get(2));
+        System.out.println(lista.get(3));
+        System.out.println(lista.get(4));
+        System.out.println(lista.remove(2));
+        System.out.println(lista);
+    }
+}
