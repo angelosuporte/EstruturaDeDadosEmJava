f22935d57b177b044d84f76cc90c3b6c2cac4f71
Angelo Carvalho <angelosuporteti@gmail.com>
2021-10-19 10:44:29 -0300

    Projeto lista circular

---
+++ ProjetoListaCircular/src/main/java/com/projetocircular/Main.java
@@ -0,0 +1,28 @@
+package com.projetocircular;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaCircular<String> circular = new ListaCircular<>();
+
+        circular.add("c0");
+        System.out.println(circular);
+
+        circular.remove(0);
+        System.out.println(circular);
+
+        circular.add("c1");
+        System.out.println(circular);
+        circular.add("c2");
+        circular.add("c3");
+
+        System.out.println(circular);
+
+        System.out.println("Verificando loop por indice usando método get");
+
+        System.out.println(circular.get(0));
+        System.out.println(circular.get(1));
+        System.out.println(circular.get(2));
+        System.out.println(circular.get(3));
+        
+    }
+}
